8b653bc678568430dd030a28758fbfee5ce22dd1
oladeleseyi <toothy61@gmail.com>
2021-12-23 12:54:07 +0100

    Added Api routes

---
+++ api/chainInfo/functions/handler.py
@@ -0,0 +1,11 @@
+import json
+
+def main(event, context):
+  body = {
+    "message": "Go Serverless v1.0! Your function executed successfully!",
+  }
+
+  return {
+      "statusCode": 200,
+      "body": json.dumps(body)
+  }
