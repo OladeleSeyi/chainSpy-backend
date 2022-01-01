ed7414d3c9dc99d2f3f011265f120cc3dde5a61f
oladeleseyi <toothy61@gmail.com>
2022-01-01 15:46:18 +0100

    univeersal error handling

--- api/chainInfo/handler.py
+++ api/chainInfo/handler.py
@@ -1,11 +1,18 @@
 import json
 
-def main(event, context):
-  body = {
-    "message": "Go Serverless v1.0! Your function executed successfully!",
-  }
 
-  return {
-      "statusCode": 200,
-      "body": json.dumps(body)
-  }
+def main(event, context):
+    try:
+        body = {
+            "message": "Go Serverless v1.0! Your function executed successfully!",
+        }
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+    except:
+        return {
+            "statusCode": 500,
+            "body": "An error occured"
+        }
